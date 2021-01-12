5e9df0a7b059163513d63158bb7f62974b8dc3c0
hyun <qkrgustnq@naver.com>
2021-01-13 08:54:41 +0900

    ing...

--- makeMVCFramework/core/Router.php
+++ makeMVCFramework/core/Router.php
@@ -27,6 +27,11 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
         $path = $this->request->getPath();
