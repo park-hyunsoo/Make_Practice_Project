4d220d8c8348c9631a4398cffe7733375c4ab11a
hyun <qkrgustnq@naver.com>
2021-01-14 16:53:08 +0900

    testing

---
+++ 5.tree_js/src/tree.js
@@ -0,0 +1,10 @@
+if(!tree){
+    /**
+    * @namespace
+    */
+    var tree={};
+    tree.model = tree.model || {};
+    tree.collection = tree.collection || {};
+    tree.view = tree.view || {};
+}
+  
