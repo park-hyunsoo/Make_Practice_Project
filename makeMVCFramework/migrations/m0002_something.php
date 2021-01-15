01b35c6c5b9fbb6c9ce5831c5f4db26a92e111e1
hyun <qkrgustnq@naver.com>
2021-01-15 18:48:53 +0900

    migrations develop

--- makeMVCFramework/migrations/m0002_something.php
+++ makeMVCFramework/migrations/m0002_something.php
@@ -4,11 +4,11 @@ class m0002_something
 {
     public function up()
     {
-        echo "Applying migration";
+        echo "Applying migration".PHP_EOL;
     }
 
     public function down()
     {
-        echo "Down migration";
+        echo "Down migration".PHP_EOL;
     }
 }
